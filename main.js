26bf7ebaf1acd75a324f630f9590f643a44ded84
moeIsAwesome <musavimoe@gmail.com>
2022-02-11 00:00:14 +0330

    project started

---
+++ main.js
@@ -0,0 +1,22 @@
+const btn = document.getElementById('jokeBtn');
+const text = document.getElementById('text');
+
+const loading = document.getElementById('shimmer');
+
+const url = 'https://icanhazdadjoke.com/';
+
+async function getter() {
+  text.style.display = 'none';
+  loading.style.display = 'block';
+  const initialPromise = await fetch(url, {
+    headers: {
+      Accept: 'application/json',
+    },
+  });
+  const myPromise = await initialPromise.json();
+  text.innerHTML = myPromise.joke;
+  loading.style.display = 'none';
+  text.style.display = 'block';
+}
+loading.style.display = 'none';
+btn.addEventListener('click', getter);
